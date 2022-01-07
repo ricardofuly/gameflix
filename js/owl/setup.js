f296946575a411f4f70cbf40e6c30f231d3ba456
Ricardo Fuly <89115052+ricardofuly@users.noreply.github.com>
2022-01-07 12:16:29 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+var owl = $('.autoplay');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
